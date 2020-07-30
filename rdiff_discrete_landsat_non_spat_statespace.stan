c9050885927756369c6e8b9d721f38fbb1dfb1f9
[Andrii Zaiats] <[andriizaiats@u.boisestate.edu]>
2020-07-30 11:57:24 -0600

    initial commit

---
+++ rdiff_discrete_landsat_non_spat_statespace.stan
@@ -0,0 +1,79 @@
+data {
+  int N;  // number of observations in the matrix
+  int T;  // number of time steps in a series
+  int N_pred;
+  int T_pred;
+  vector[N] y_mat[T]; // response, cover ts
+  vector[N] y0_tr;
+  vector[N_pred] y0_pred;
+  vector[N] y_k_prior;
+  vector[N_pred] y_k_pred;
+} transformed data {
+  vector[N] y_k_prior_sc = (y_k_prior - mean(y_k_prior)) ; //./ 2*sd(y_k_prior)
+  vector[N_pred] y_k_pred_sc = (y_k_pred - mean(y_k_pred)) ; //./ 2*sd(y_k_pred)
+} parameters {
+  real alpha;
+  real alpha_pred;
+  real<lower=0> sigma_alpha_pred;
+  real<lower=0,upper=1> r;          // global growt rate
+  real<lower=0,upper=1> r_mu;       // avg growth 
+  real<lower=0> r_sigma;    // growth variance
+  vector<lower=0>[N] k;     // pixel-level carrying capacity
+  real<lower=0> k_mu;       // average (global) k
+  real<lower=0> k_sigma;    // k variance 
+  real k_phi;               // change in post-burn caryying capacity
+  vector<lower=0>[T] eta;   // observation error
+  real<lower=0> gamma;        // observation error
+  
+  vector<lower=0,upper=1>[N] z_init;          // initial state vector (non-dim in k)
+  vector<lower=0,upper=1>[N_pred] z_pred;     // initial state vector (non-dim in k)
+  
+  vector<lower=0>[N] Z[T+1]; // latent states
+} transformed parameters {
+
+} model {
+  // priors: intercempts
+  alpha ~ normal(0,1);
+  alpha_pred ~ normal(0,10);
+  sigma_alpha_pred ~ exponential(1);
+  // priors: demographic parameters
+  r_mu ~ normal(.5,1)T[0,1];
+  r_sigma ~ exponential(5);
+  r ~ normal(r_mu,r_sigma);
+  k_mu ~ normal(0, 5);
+  k_sigma ~ exponential(1);
+  k_phi ~ normal(0,.1)T[-1,1];
+  eta ~ exponential(1); 
+  gamma ~ exponential(1);
+  
+  for(i in 1:N) z_init[i] ~ lognormal(0,1);
+  for(i in 1:N_pred) z_pred[i] ~ lognormal(0,1);
+  
+  Z[1] ~ normal(z_init,gamma); 
+  for(t in 2:T+1)
+      Z[t] ~ normal(Z[t-1] + r*Z[t-1] - r*(Z[t-1] .* Z[t-1]),gamma);
+  
+  k ~ normal(k_mu + k_phi*y_k_prior_sc,k_sigma);
+  y_k_pred ~ normal(alpha_pred,sigma_alpha_pred);
+
+  y0_tr ~ normal(z_init,eta[1]);
+  y0_pred ~ normal(z_pred,.1);
+   
+   for(t in 1:T){
+      y_mat[t] ~ normal(alpha + Z[t] .* k,eta[t]);
+   }
+} generated quantities {
+  matrix[T_pred,N_pred] y_pred01;
+  matrix[T_pred,N_pred] y_pred;
+  //vector<lower=0>[N_pred] k_pred = mean(y_k_pred) + k_phi*y_k_pred_sc;
+
+  for(i in 1:N_pred)  y_pred01[1,i] = z_pred[i];
+  
+  for(t in 2:T_pred){
+    for(i in 1:N_pred)
+      y_pred01[t,i] = (y_pred01[t-1,i]+ r * y_pred01[t-1,i] - r * y_pred01[t-1,i]^2);
+    }
+  for(t in 1:T_pred) 
+    y_pred[t] = alpha + y_pred01[t] .* (alpha_pred + k_phi * y_k_pred_sc');
+    
+}
---
+++ spatiotemp_sim.R
@@ -0,0 +1,155 @@
+ ###### Spatio temporal data simulation
+library(raster)
+library(ggplot2)
+library(dplyr)
+library(viridisLite)
+library(reshape2)
+
+neighborhood <- function(raster){
+  nn <- matrix(NA,length(raster[]),4)
+  for(i in 1:dim(nn)[1]){
+    loc <- adjacent(raster,i)[,2]
+    ln <- loc[which((loc+1)==i)]
+    rn <- loc[which((loc-1)==i)]
+    bn <- loc[which((loc-dim(raster)[2])==i)]
+    tn <- loc[which((loc+dim(raster)[2])==i)]
+    nn[i,1] <- ifelse(length(ln)>0,ln,0)
+    nn[i,2] <- ifelse(length(rn)>0,rn,0)
+    nn[i,3] <- ifelse(length(tn)>0,tn,0)
+    nn[i,4] <- ifelse(length(bn)>0,bn,0)
+  }
+  nn
+}
+wrap = function(x,poly){
+  row=dim(poly)[1]
+  col=dim(poly)[2]
+  mat=matrix(x, row,col, byrow=TRUE)
+  mat
+}
+
+### - a function of discretized Fickian diffusion
+makeH <- function(NN, Dvec, dx, dy){
+  H <- matrix(0, dim(NN)[1], dim(NN)[1])
+  diag(H) <- Dvec * (1 / dx^2 + 1 / dy^2)
+  for(i in 1:dim(H)[1]){
+    H[i,i] = Dvec[i]*(1/dx^2 + 1/dy^2)
+    if(sum(NN[i, ] != 0) == 4){
+      if(NN[i, 1] > 0){H[i, NN[i, 1]] <- (Dvec[i] - (Dvec[NN[i, 2]] - Dvec[NN[i, 1]]) / 4) / (dx^2)}
+      if(NN[i, 2] > 0){H[i, NN[i, 2]] <- (Dvec[i] + (Dvec[NN[i, 2]] - Dvec[NN[i, 1]]) / 4) / (dx^2)}
+      if(NN[i, 3] > 0){H[i, NN[i, 3]] <- (Dvec[i] + (Dvec[NN[i, 4]] - Dvec[NN[i, 3]]) / 4) / (dy^2)}
+      if(NN[i, 4] > 0){H[i, NN[i, 4]] <- (Dvec[i] - (Dvec[NN[i, 4]] - Dvec[NN[i, 3]]) / 4) / (dy^2)}
+    }}
+  H
+}
+makeG <- function(NN, uvec , dx, dy){
+  G = matrix(0,dim(NN)[1],dim(NN)[1])
+  for(i in 1:dim(H)[1]){
+    if(sum(NN[i,]!=0) == 4){
+      G[i,i] <- (uvec[NN[i,2]] - 2*uvec[i] + uvec[NN[i,1]])/(2*dx^2) + (uvec[NN[i,4]] - 2*uvec[i] + uvec[NN[i,3]])/(2*dy^2)
+      if(NN[i,1]>0){H[i,NN[i,1]] <- -(uvec[NN[i,2]] - uvec[NN[i,1]])/(4*dx^2)}
+      if(NN[i,2]>0){H[i,NN[i,2]] <- (uvec[NN[i,2]] - uvec[NN[i,1]])/(4*dx^2)}
+      if(NN[i,3]>0){H[i,NN[i,3]] <- -(uvec[NN[i,3]] - uvec[NN[i,3]])/(4*dy^2)}
+      if(NN[i,4]>0){H[i,NN[i,4]] <- (uvec[NN[i,4]] - uvec[NN[i,3]])/(4*dy^2)}}
+  }
+  G
+}
+
+### - a function of discretized Plain diffusion with varying D
+makeGplain <- function(NN, uvec , dx, dy, dt){
+  G <- matrix(0, dim(NN)[1], dim(NN)[1])
+  for(i in 1:dim(G)[1]){
+    G[i,i] <- -4 * uvec[i]
+    if(sum(NN[i, ] != 0) == 4){
+      if(NN[i, 1] > 0){G[i, NN[i, 1]] <- uvec[NN[i,1]]}
+      if(NN[i, 2] > 0){G[i, NN[i, 2]] <- uvec[NN[i,2]]}
+      if(NN[i, 3] > 0){G[i, NN[i, 3]] <- uvec[NN[i,3]]}
+      if(NN[i, 4] > 0){G[i, NN[i, 4]] <- uvec[NN[i,4]]}}
+  }
+  G *(dt / (dx^2 + dy^2))
+}
+
+# --- load data and transform to N x T formal
+# years <- c(1985:2017)
+# path <- "~/MEGA/landsat_backintime/"
+# file <- "BogusCreek_1986_clipped_30_30.tif" #1986
+# ras <- stack(paste0(path,file))
+# poly <- subset(ras,1:14)
+# values(poly)[values(poly) == 101] <- NA
+# df1 <- as.data.frame(poly, xy = TRUE, na.rm = TRUE)
+# names(df1) <- c('x', 'y', paste0('year', years[1:14]))
+
+n <- 50
+T <- 24
+matsim <- matrix(.01, n, n)
+NN <- neighborhood(raster(matsim))
+ymat <- as.data.frame(matsim, xy = FALSE, na.rm = TRUE)
+
+dx <- 30
+dy <- 30
+dt <- 1
+
+r <- .2
+k <- 15
+D <- rep(250, n^2)
+G <- makeGplain(NN, y0 , dx, dy, dt)
+
+matsim[1:3, ] <- k
+ysim <- matrix(.01, n^2, T)
+ysim[, 1] <- as.numeric(matsim)
+ysim <- ysim/k
+simsage <- function(ysim, r, k, D, dx, dy, dt, NN, spat = TRUE) {
+  if(sum((ysim[, 1] > k)) > 0){
+    return("error")
+  } else{
+    for(t in 2:ncol(ysim)){
+      print(t)
+      if(spat == TRUE){
+        G = makeGplain(NN, ysim[, t-1], dx, dy, dt)
+        ysim[, t] =  ysim[, t-1] + G %*% D + r * ysim[, t-1] * (1 -  ysim[, t-1]) 
+      } else {
+        ysim[, t] =  ysim[, t-1] + r * ysim[, t-1] * (1 -  ysim[, t-1])
+      }
+    }
+  return(ysim)
+  }
+}
+
+ysim <- simsage(ysim = ysim, r = r, k = k, D = D, 
+               dx = dx, dy = dy, dt = dt, NN = NN,
+               spat = TRUE)
+
+matplot(t(ysim), type="l" , ylab="Cover, %", xlab="Time, years", col = viridis(n))
+# --- plot raster time-series
+par(mfrow=c(3,3),mar=c(1,1,1,1))
+for(i in 1:T){
+  ymat=wrap(ysim[,i],raster(matsim))
+  plot(raster(ymat))
+  #plot(poly[[i]])
+}
+dev.off()
+# ---------------------------------------------------
+Dvec <- seq(0, 300, l = 9)
+D <- matrix(NA, n^2, T)
+dt = seq(1/365, 1, l = 9)
+par(mfrow = c(3, 3), mar = c(1, 1, 1, 1))
+for(i in 1:length(Dvec)){
+  D[, i] = Dvec[i]
+  ysim <- simsage(ysim = ysim, r = .5, k = k, D = rep(50, n^2), 
+                  dx = dx, dy = dy, dt = dt[1], NN = NN,
+                  spat = TRUE)
+  matplot(t(ysim), type="l" , ylab="Cover, %", xlab="Time, years", col = viridis(n))
+}
+
+# === linearized models
+dat <- melt(ysim)
+names(dat)=c("id","year","cover")
+plot(1,type="n",xlim=c(0,max(dat$year)),ylim=c(-2,4))
+for(i in 1:nrow(ysim)){
+  points(lcover~year,data=dat[dat$id==i,],type="l")
+}
+dat$lcover = log(dat$cover)
+lm(dat$lcover~log(dat$year))
+curve(-.75+1.5*log(x),0,12,col="red",lwd=5,add=TRUE)
+points(1,log(mean(ysim[,1])),col="green",cex=2,pch=19)
+
+
