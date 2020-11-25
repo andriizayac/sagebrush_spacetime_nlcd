a6d1487923dddfbaeaca56e6da07198a18464162
Andrii Zaiats <andriizaiats@u.boisestate.edu>
2020-11-25 14:14:15 -0700

    stan code just fitting model, no predictions, data prep fn accordingly

--- functions.R
+++ functions.R
@@ -8,11 +8,25 @@ mae <- function(x, y){
 }
 
 # plot the ts of average shrub values with proposed fire time point
-fig <- function(dat = sagemeans, index = dfspat$closeMatch, offs = offset, i = k, years = yrs){
-  plot(dat[index[i],],type="b",main=paste0("id=",index[i],": offset=",offs[index[i]]))
-  abline(v=years[index[i]]+offs[index[i]],lwd=2,col="red")
+fig <- function(dat = sagemeans, index = dfspat$idvar, offs = offset, i = k, years = yrs){
+  plot(dat[i,],type="b",main=paste0("id=",index[i],": offset=",offs[index[i]], " year: ", dfspat$FireYear[i]))
+  abline(v=years[i],lwd=2,col="red") #+offs[index[i]]
   k <<- k+1
 }
+
+# subset a list by logical
+subsetList <- function(alist, var){
+  slist = list()
+  k = 1
+  for(i in 1:length(alist)){
+    if(var[i] == TRUE){
+      slist[[k]] = alist[[i]]
+    k = k+1
+    }
+  }
+  return(slist)
+}
+  
 
 # plot pre- and post- fire rasters (based on the dfspat - matched data frame) 
 plotfire <- function(id = NULL, offyr = offset, focal = TRUE, pol = dfspat, rast = sage){
@@ -270,10 +284,64 @@ dataprep2 <- function(n = NULL, pol = NULL, rast = NULL, years = NULL, yroffset 
   return(dlist)
 }
 
+# prepare input data for non-spatial stan model without predictions
+dataprep3 <- function(alist, k, df, tr_prop = .5, thresholdval = 1){
+  rast = alist[[k]]
+  fyear = dfspat$FireYear[k]
+  # subset post-disturbance trajectories
+  dftr <- as.data.frame(rast, xy = F, na.rm = T)[, years >= fyear]
+  # create training indexes
+  tr <- sample(1:nrow(dftr), round(nrow(dftr) * tr_prop))
+  # create training and test datasets, create pre-disutrbance means
+  y0_train <- dftr[tr, 1]
+  y0_pred <- dftr[-tr, 1]
+  
+  # --- this if-else creates a value for pre-disturbance cover: previous year OR averaged since the beginning of time series
+  if(sum(years < fyear - 1) == 1) {
+    y_k_prior <- as.numeric(as.data.frame(rast, xy = F, na.rm = T)[tr, 1:sum(years < fyear)])
+    y_k_pred <- as.numeric(as.data.frame(rast, xy = F, na.rm = T)[-tr, 1:sum(years < fyear)])
+  } else {
+    y_k_prior <- as.numeric(apply(as.data.frame(rast, xy = F, na.rm = T)[tr, 1:sum(years < fyear[1])], 1, mean))
+    y_k_pred <- as.numeric(apply(as.data.frame(rast, xy = F, na.rm = T)[-tr, 1:sum(years < fyear[1])], 1, mean))
+  }
+  
+  # standardize initial values to [0,1] for non-dimensional model. scales either to a prop of x/k or returns 1 
+  y0_tr <- ifelse(y0_train < y_k_prior, y0_train / y_k_prior, y0_train / y0_train)
+  y0_pr <- ifelse(y0_pred < y_k_pred, y0_pred / y_k_pred, y0_pred / y0_pred)
+
+  # --- this part allows to use only the pixels with initial values below a threshold (whithin equilibria \in [0, 1])
+    y0_train <- ifelse(is.finite(y0_tr), y0_tr, 0)
+    y0_pred <- ifelse(is.finite(y0_pr), y0_pr, 0)
+    idx <- which(y0_train < thresholdval)
+    # --- adds increments the threshold up in case fewer that 100 pixes fall under the threshold 
+    while(length(idx) < 100) {
+      thresholdval <- thresholdval + .1
+      idx <- which(y0_train < thresholdval)
+    }
+    y_matrix <- as.matrix(dftr[tr, ])[idx, ]
+    
+    dlist <- list(N = nrow(y_matrix),
+                  tr = tr,
+                  T = ncol(dftr),
+                  # training data
+                  y_mat=t(y_matrix),
+                  y_k_prior = y_k_prior[idx],
+                  y0 = y0_train[idx],
+                  # prediction data
+                  n = nrow(dftr[-tr, ]),
+                  y0pred = y0_pred,
+                  y_k_pred = y_k_pred,
+                  # record threshold used
+                  cover_threshold = thresholdval,
+                  # input data sets
+                  dftr=as.data.frame(y_matrix))
+
+  return(dlist)
+}
 
 
-
-i = 2
+i = 1
+alist1 <- dataprep3(sage,1,dfspat,.15,.25)
 alist1 <- dataprep(n = i, pol = dfspat, rast = sage, year = years, yroffset = offset, seed = 123,
                   trsubset = .1,
                   predsubset = 1,threshold = TRUE,thresholdval = .1)
